ba545056daa83d97ca4c486f14dee8abfb095477
Steven Silvester <steven.silvester@ieee.org>
2015-08-07 16:46:36 -0500

    First successful ajax test

--- tests/src/testkernel.ts
+++ tests/src/testkernel.ts
@@ -9,21 +9,70 @@
 module tests {
 
 import Kernel = jupyter.services.Kernel;
+import IKernelId = jupyter.services.IKernelId;
+declare var sinon: any;
+
+
+class RequestHandler {
+
+  constructor() {
+    this._xhr = sinon.useFakeXMLHttpRequest();
+    this._xhr.onCreate = (xhr: any) => {
+      this._requests.push(xhr);
+    }
+  }
+
+  respond(statusCode: number, header: any, data: any): void {
+    this._requests[0].respond(statusCode, header, data);
+  }
+
+  restore(): void {
+    this._xhr.restore();
+  }
+
+  private _requests: any[] = [];
+  private _xhr: any = null;
+}
 
 
 describe('jupyter.services', () => {
 
   describe('Kernel', () => {
 
-    describe('#construct()', () => {
+    describe('#constructor()', () => {
 
-      it('should return true on success', () => {
+      it('should set initial conditions', () => {
         var kernel = new Kernel('baseUrl', 'wsUrl');
         expect(kernel.name).to.be("unknown");
+        kernel.name = "test";
+        expect(kernel.name).to.be("test");
+        expect(kernel.status).to.be("unknown");
+        expect(kernel.isConnected).to.be(false);
       });
 
     });
+
+    describe('#getInfo()', () => {
+
+      it('should yield a valid kernel id', () => {
+        var kernel = new Kernel('baseUrl', 'wsUrl');
+        var handler = new RequestHandler();
+        var info = kernel.getInfo();
+        var data = JSON.stringify({id: "1234", name: "test"});
+        handler.respond(200, { 'Content-Type': 'text/json' }, data);
+        return info.then((response: IKernelId) => {
+          expect(response.name).to.be("test");
+          expect(response.id).to.be("1234");
+          handler.restore();
+        });
+        
+      });
+
+    });
+
    });
+
+
 });
 
 }  // module tests
