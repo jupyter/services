b3f35e8c7c1ce5a670d0d434e4ce3eaf244400fb
Steven Silvester <steven.silvester@ieee.org>
2015-08-14 08:51:31 -0500

    Update license

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,6 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
 
-/*-----------------------------------------------------------------------------
-| Copyright (c) 2014-2015, S. Chris Colbert
-|
-| Distributed under the terms of the BSD 3-Clause License.
-|
-| The full license is in the file LICENSE, distributed with this software.
-|----------------------------------------------------------------------------*/
 'use strict';
 
 var del = require('del');
