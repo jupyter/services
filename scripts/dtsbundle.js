8b8c195a0e30cf999837bfc07725ab33f143677c
Steven Silvester <steven.silvester@ieee.org>
2015-08-14 10:00:43 -0500

    Start removing gulp

---
+++ scripts/dtsbundle.js
@@ -0,0 +1,11 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2015, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+require('dts-bundle').bundle({
+  name: 'jupyter-js-services',
+  main: 'lib/index.d.ts',
+});
