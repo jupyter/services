3df07ba32e2efce7054bad650c3ac39327377d33
Steven Silvester <steven.silvester@ieee.org>
2015-09-08 15:56:04 -0500

    Clean up karma test

--- test/run_test.py
+++ test/run_test.py
@@ -5,20 +5,21 @@ import subprocess
 import sys
 import argparse
 
+KARMA_PORT = 9876
 
 argparser = argparse.ArgumentParser(
         description='Run Jupyter JS Sevices integration tests'
     )
 argparser.add_argument('-b', '--browsers', default='Firefox',
-                       help="Browser to use for Karma test")
+                       help="Browsers to use for Karma test")
 options = argparser.parse_args(sys.argv[1:])
 
 nb_command = [sys.executable, '-m', 'notebook', '--no-browser',
-              '--NotebookApp.allow_origin="http://localhost:9876"']
+              '--NotebookApp.allow_origin="http://localhost:%s"' % KARMA_PORT]
 nb_server = subprocess.Popen(nb_command, stderr=subprocess.STDOUT)
 
 karma_command = ['karma', 'start', '--browsers=' + options.browsers,
-                 'karma.conf.js']
+                 'karma.conf.js', '--port=%s' % KARMA_PORT]
 try:
     resp = subprocess.check_call(karma_command, stderr=subprocess.STDOUT)
 except subprocess.CalledProcessError:
