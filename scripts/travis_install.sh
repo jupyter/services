b97465d390efbdb5a7f070c7a7a2e728c5452c4f
Steven Silvester <steven.silvester@ieee.org>
2015-11-04 08:52:53 -0600

    Manually install pip

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 npm install
-wget https://repo.continuum.io/miniconda/Miniconda-latest-Linux-x86_64.sh -O miniconda.sh;
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
 bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
+conda install pip
 
 # install development version of notebook
 # https://github.com/jupyter/notebook/blob/master/.travis.yml
