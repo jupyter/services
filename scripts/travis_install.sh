50738c7a8f41358383cdd54ea0f0f47e0a3c6023
Steven Silvester <steven.silvester@ieee.org>
2016-04-12 10:13:08 -0500

    Ensure latest notebook

--- scripts/travis_install.sh
+++ scripts/travis_install.sh
@@ -8,7 +8,7 @@ hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
-conda install pip notebook
+conda install pip pyzmq
 # Install the latest version of the notebook
 pip install --pre notebook
 
