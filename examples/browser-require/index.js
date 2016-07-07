db26a430f61de607e5266e3a552f0925958b54a6
Steven Silvester <steven.silvester@ieee.org>
2016-07-07 14:51:09 -0500

    Minor cleanup of browser-require

--- examples/browser-require/index.js
+++ examples/browser-require/index.js
@@ -1,6 +1,6 @@
 
 require(['jquery', 'jupyter-js-services'], function ($, services) {
-  "uses strict";
+  'use strict';
   var startNewKernel = services.startNewKernel;
 
   var kernelOptions = {
@@ -9,27 +9,27 @@ require(['jquery', 'jupyter-js-services'], function ($, services) {
 
   // start a single kernel for the page
   startNewKernel(kernelOptions).then(function (kernel) {
-    console.log('kernel started', kernel);
+    console.log('Kernel started:', kernel);
     kernel.kernelInfo().then(function (reply) {
       var content = reply.content;
-      $("#kernel-info").text(content.banner);
-      console.log(info);
+      $('#kernel-info').text(content.banner);
+      console.log('Kernel info:', content);
     })
-    $("#run").click(function () {
-      var code = $("#cell").val();
-      console.log("executing", code);
+    $('#run').click(function () {
+      var code = $('#cell').val();
+      console.log('Executing:', code);
       // clear output
-      $("#output").text("");
+      $('#output').text('');
       // Execute and handle replies on the kernel.
       var future = kernel.execute({ code: code });
       // record each IOPub message
       future.onIOPub = function (msg) {
-        console.log('Got IOPub', msg);
-        $("#output").append(
-          $("<pre>").text("msg_type: " + msg.header.msg_type)
+        console.log('Got IOPub:', msg);
+        $('#output').append(
+          $('<pre>').text('msg_type: ' + msg.header.msg_type)
         );
-        $("#output").append(
-          $("<pre>").text(JSON.stringify(msg.content))
+        $('#output').append(
+          $('<pre>').text(JSON.stringify(msg.content))
         );
       };
 
@@ -39,7 +39,7 @@ require(['jquery', 'jupyter-js-services'], function ($, services) {
 
       future.onDone = function () {
         console.log('Future is fulfilled');
-        $("#output").append($("<pre>").text("Done!"));
+        $('#output').append($('<pre>').text('Done!'));
       };
     });
   });
