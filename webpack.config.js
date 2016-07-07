daa89751125bd9d00502f80fbae96fcd032092e2
Steven Silvester <steven.silvester@ieee.org>
2016-07-07 06:42:24 -0500

    Unify the library name and export the amd name

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,9 @@ module.exports = {
     entry: './lib',
     output: {
         filename: './dist/index.js',
-        library: ['jupyter', 'services'],
+        library: 'jupyter-js-services',
         libraryTarget: 'umd',
+        umdNamedDefine: true,
         publicPath: 'https://npmcdn.com/jupyter-js-services@' + version + '/dist/'
     },
     devtool: 'source-map'
