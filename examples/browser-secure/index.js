95a26861bbb364a153b7bf12e7a25e5c61be1676
Steven Silvester <steven.silvester@ieee.org>
2016-07-27 10:55:39 -0500

    wip browser-secure

--- examples/browser-secure/index.js
+++ examples/browser-secure/index.js
@@ -1,51 +1,60 @@
 
 require(['jquery', 'jupyter-js-services'], function ($, services) {
   'use strict';
-  var startNewKernel = services.startNewKernel;
 
-  var kernelOptions = {
-    name: 'python',
-    ajaxSettings: {
-      withCredentials: true,
-      user: 'foo',
-      password: 'password'
-    }
-  };
+  var baseUrl = services.utils.getBaseUrl();
+  var url = services.utils.urlPathJoin(baseUrl, 'login');
+  services.utils.ajaxRequest(url, {
+    data: JSON.stringify({ password: 'password' }),
+    method: 'POST',
+    contentType: 'application/json'
+  }).then(function (success) {
 
-  // start a single kernel for the page
-  startNewKernel(kernelOptions).then(function (kernel) {
-    console.log('Kernel started:', kernel);
-    kernel.kernelInfo().then(function (reply) {
-      var content = reply.content;
-      $('#kernel-info').text(content.banner);
-      console.log('Kernel info:', content);
-    })
-    $('#run').click(function () {
-      var code = $('#cell').val();
-      console.log('Executing:', code);
-      // clear output
-      $('#output').text('');
-      // Execute and handle replies on the kernel.
-      var future = kernel.execute({ code: code });
-      // record each IOPub message
-      future.onIOPub = function (msg) {
-        console.log('Got IOPub:', msg);
-        $('#output').append(
-          $('<pre>').text('msg_type: ' + msg.header.msg_type)
-        );
-        $('#output').append(
-          $('<pre>').text(JSON.stringify(msg.content))
-        );
+      var startNewKernel = services.startNewKernel;
+
+      var kernelOptions = {
+        name: 'python',
+        ajaxSettings: {
+          withCredentials: true,
+          password: 'password'
+        }
       };
 
-      future.onReply = function (reply) {
-        console.log('Got execute reply');
-      };
+    // start a single kernel for the page
+    startNewKernel(kernelOptions).then(function (kernel) {
+      console.log('Kernel started:', kernel);
+      kernel.kernelInfo().then(function (reply) {
+        var content = reply.content;
+        $('#kernel-info').text(content.banner);
+        console.log('Kernel info:', content);
+      })
+      $('#run').click(function () {
+        var code = $('#cell').val();
+        console.log('Executing:', code);
+        // clear output
+        $('#output').text('');
+        // Execute and handle replies on the kernel.
+        var future = kernel.execute({ code: code });
+        // record each IOPub message
+        future.onIOPub = function (msg) {
+          console.log('Got IOPub:', msg);
+          $('#output').append(
+            $('<pre>').text('msg_type: ' + msg.header.msg_type)
+          );
+          $('#output').append(
+            $('<pre>').text(JSON.stringify(msg.content))
+          );
+        };
 
-      future.onDone = function () {
-        console.log('Future is fulfilled');
-        $('#output').append($('<pre>').text('Done!'));
-      };
+        future.onReply = function (reply) {
+          console.log('Got execute reply');
+        };
+
+        future.onDone = function () {
+          console.log('Future is fulfilled');
+          $('#output').append($('<pre>').text('Done!'));
+        };
+      });
     });
   });
 });
