c26bb3aba9d96cb34d7050a292f2eabfeeefe09d
Jason Grout <jgrout6@bloomberg.net>
2016-10-12 16:47:50 -0400

    Add url typings.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,6 +1,6 @@
-
 /// <reference path="../typings/path-posix/path-posix.d.ts"/>
 /// <reference path="../typings/url-join/url-join.d.ts"/>
+/// <reference path="../typings/url/url.d.ts"/>
 
 /*
  * TODO: remove the below typings after typedoc understands the lib compiler option
@@ -8,7 +8,6 @@
  * When this happens, use the typescript compiler option:
  * "lib": ["dom", "es5", "es2015.promise", "es2015.collection"],
  */
-/// <reference path="../node_modules/@types/node/index.d.ts"/>
 /// <reference path="../node_modules/@types/minimist/index.d.ts"/>
 /// <reference path="../node_modules/@types/text-encoding/index.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
