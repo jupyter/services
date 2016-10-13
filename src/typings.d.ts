d04b6f733cf85031e834cab20a82e648196e0026
Steven Silvester <steven.silvester@ieee.org>
2016-10-13 09:01:03 -0500

    Avoid having an explicit node typings requirement

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -8,7 +8,6 @@
  * When this happens, use the typescript compiler option:
  * "lib": ["dom", "es5", "es2015.promise", "es2015.collection"],
  */
-/// <reference path="../node_modules/@types/node/index.d.ts"/>
 /// <reference path="../node_modules/@types/minimist/index.d.ts"/>
 /// <reference path="../node_modules/@types/text-encoding/index.d.ts"/>
 /// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
