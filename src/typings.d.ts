83fda1fe9c91c7306ad50998796a1c50e273fc02
Jason Grout <jgrout6@bloomberg.net>
2016-10-14 21:35:28 -0400

    Separate out typings needed for typedoc.

--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,3 +1,5 @@
+// Copyright (c) Jupyter Development Team.
+// Distributed under the terms of the Modified BSD License.
 
 /*
  * TODO: switch the below typings to @types packages when they are available.
@@ -5,13 +7,3 @@
 /// <reference path="../typings/path-posix/path-posix.d.ts"/>
 /// <reference path="../typings/url-join/url-join.d.ts"/>
 /// <reference path="../typings/url/url.d.ts"/>
-
-/*
- * TODO: remove the below typings after typedoc understands the lib compiler option
- * and the @types typing resolution.
- */
-/// <reference path="../node_modules/@types/text-encoding/index.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es2015.promise.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.dom.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es5.d.ts"/>
-/// <reference path="../node_modules/typescript/lib/lib.es2015.collection.d.ts"/>
