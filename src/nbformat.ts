672fa62228441f5697767c3c3a5054a8161f26bb
Steven Silvester <steven.silvester@ieee.org>
2017-02-22 03:58:25 -0600

    Fix typings

--- src/nbformat.ts
+++ src/nbformat.ts
@@ -19,13 +19,13 @@ namespace nbformat {
    * The major version of the notebook format.
    */
   export
-  const MAJOR_VERSION = 4;
+  const MAJOR_VERSION: number = 4;
 
   /**
    * The minor version of the notebook format.
    */
   export
-  const MINOR_VERSION = 1;
+  const MINOR_VERSION: number = 1;
 
   /**
    * The kernelspec metadata.
